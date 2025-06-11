9591d368b2006d70c82ad2e8c330a041d4c88090
UTastuya <2301134@s.asojuku.ac.jp>
2025-06-11 12:45:24 +0900

    篠原

--- todo.php
+++ todo.php
@@ -6,6 +6,35 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="" name="tasuku">
+        <h2>タスク追加</h2>
+        <input type="text" name="naiyou">
+        <input type="datetime" name="kare">
+        <select name="user_pref">
+            <?php
+            $prefectures = [
+                "優先度(低)",
+                "中",
+                "高"
+            ]
+            ?>
+            <input type="button" name="tui" value="追加">
+    </form>
+    <form action="" name="kensaku">
+    <input class="search_bar" type="text" name="keyword" placeholder="キーワード">
+    <?php
+            $prefectures = [
+                "優先度(全)",
+                "低",
+                "中",
+                "高"
+            ]
+            ?>
+    </form>
+    
+    <?php
+    
+    ?>
     
 
 
