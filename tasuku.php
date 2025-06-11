0c3436d5538efa1fc55cc5f00ab4f6d339c257ec
2301145 <2301145@s.asojuku.ac.jp>
2025-06-11 12:29:29 +0900

    タスク編集

---
+++ tasuku.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>タスク編集</h1>
+    内容:<input type="text" value="task"><br>
+    期限:<input type="date" value="due_date"><br>
+    優先度:
+    <select value="priority">
+        <option value="low">低</option>
+        <option value="medium">中</option>
+        <option value="high">高</option>
+    </select><br>
+    状態
+    <select value="status">
+        <option value="todo">未着手</option>
+        <option value="in_progress">進行中</option>
+        <option value="done">完了</option>
+    </select><br>
+    <button type="submit">保存</button>
+    <a href="">キャンセル</a>
+</body>
+</html>
