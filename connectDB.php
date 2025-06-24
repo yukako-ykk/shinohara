6678b0d69ac2e88291baa7fd73562c1aa253c295
yukako-ykk <2301188@s.asojuku.ac.jp>
2025-06-24 10:24:44 +0900

    変更

--- connectDB.php
+++ connectDB.php
@@ -1,23 +1,9 @@
 <?php
-
 function connectDB() {
-    $dsn = 'mysql:host=mysql305.phy.lolipop.lan;
-                dbname=LAA1553915-tododb;charset=utf8';
+    $dsn = 'mysql:host=mysql320.phy.lolipop.lan;dbname=LAA1553915-tododb;charset=utf8';
     $username = 'LAA1553915';
     $password = 'shinohara';
 
     $pdo = new PDO($dsn, $username, $password);
     return $pdo;
 }
-
-function connectDB_local() {
-    $dsn = 'mysql:host=localhost;
-    dbname=tododb;charset=utf8';
-    $username = 'root';
-    $password = 'root';
-
-    $pdo = new PDO($dsn, $username, $password);
-    return $pdo;
-}
-
-?>  
