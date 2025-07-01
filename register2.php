71d2a12bf7ceec15b9eeaa1145a9a058a805e051
UTastuya <2301134@s.asojuku.ac.jp>
2025-07-01 10:15:47 +0900

    kjh

--- register2.php
+++ register2.php
@@ -1,6 +1,6 @@
 <?php session_start(); ?>
 <h2>ユーザー登録</h2>
-<form action="register_process2.php" method="post">
+<form action="login2.php" method="post">
     ユーザー名：<input type="text" name="username"><br>
     パスワード：<input type="password" name="password"><br>
     <button type="submit">登録</button>
